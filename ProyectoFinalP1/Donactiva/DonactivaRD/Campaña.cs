10240a6b7fa0df4fd0a3c62ab237cb130a003409
Johan Manuel Vicente B <jomavi582@gmail.com>
2025-04-04 22:38:49 -0400

    Finishing Update Class

--- ProyectoFinalP1/Donactiva/DonactivaRD/Campaña.cs
+++ ProyectoFinalP1/Donactiva/DonactivaRD/Campaña.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
 namespace DonactivaRD
 {
     public class Campaña
     {
+        [Key]
         public int Id { get; set; }
         public string Nombre { get; set; }
+        [MaxLength(100)]
         public string Descripcion { get; set; }
+        [MaxLength(100)]
         public DateTime FechaInicio { get; set; }
         public DateTime FechaFin { get; set; }
         public decimal Meta { get; set; }
         public string Estado { get; set; } // Activa, Finalizada, Cancelada
+        
     }
 }
--- ProyectoFinalP1/Donactiva/DonactivaRD/Donacion.cs
+++ ProyectoFinalP1/Donactiva/DonactivaRD/Donacion.cs
@@ -7,12 +7,17 @@ using System.ComponentModel.DataAnnotations;
 
 namespace DonactivaRD
 {
+
     public class Donacion
     {
+        [Key]
         public int Id { get; set; }
         public string Tipo { get; set; } // Monetaria, Especie o de Tiempo
+        [MaxLength(50)]
         public string Descripcion { get; set; }
+        [MaxLength(100)]
         public decimal Monto { get; set; }
+        
         public DateTime Fecha { get; set; }
         public int DonanteId { get; set; }
         public int OrganizacionId { get; set; }
