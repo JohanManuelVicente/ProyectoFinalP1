7906fc4482d061e07695100fa0a737c758761b1e
Johan Manuel Vicente B <jomavi582@gmail.com>
2025-04-04 22:08:43 -0400

    Update Donantes.cs

--- ProyectoFinalP1/Donactiva/DonactivaRD/Donantes.cs
+++ ProyectoFinalP1/Donactiva/DonactivaRD/Donantes.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
 namespace DonactivaRD
 {
     public class Donante
     {
+        [Key]
         public int Id { get; set; }
+        //[MaxLength(100)]
         public string Nombre { get; set; }
+        [MaxLength(100)]
         public string Correo { get; set; }
+        [MaxLength(150)]
         public string Telefono { get; set; }
+        [MaxLength(20)]
         public string Cedula { get; set; }
+        [MaxLength(15)]
         public string Direccion { get; set; }
+        [MaxLength(100)]
         public string Tipo { get; set; } // Individual o Empresa
+        
     }
 }
