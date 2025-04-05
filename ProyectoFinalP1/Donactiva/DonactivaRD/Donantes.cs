1a862c43bd4a3d0a89baea4b331bc4025bf26120
Johan Manuel Vicente B <jomavi582@gmail.com>
2025-04-04 21:26:52 -0400

    Corrijiendo la clase donantes y creando la clase donaciones

--- ProyectoFinalP1/Donactiva/DonactivaRD/Donantes.cs
+++ ProyectoFinalP1/Donactiva/DonactivaRD/Donantes.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DonactivaRD
 {
-    public class Donantes
+    public class Donante
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
