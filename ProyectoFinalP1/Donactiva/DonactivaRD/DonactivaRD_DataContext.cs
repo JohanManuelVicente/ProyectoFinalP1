e7c09d93b1bdaf053ae1accea7fd2522f045a74d
Johan Manuel Vicente B <jomavi582@gmail.com>
2025-04-11 19:40:18 -0400

    Update DataContext

--- ProyectoFinalP1/Donactiva/DonactivaRD/DonactivaRD_DataContext.cs
+++ ProyectoFinalP1/Donactiva/DonactivaRD/DonactivaRD_DataContext.cs
@@ -9,6 +9,10 @@ namespace DonactivaRD
 {
     public class DonactivaRD_DataContext: DbContext
     {
-
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=VICENTE\\SQLEXPRESS;Database=DonactivaRD;Trusted_Connection=True;MulptipleActiveResultSets=true;TrustServerCertificate=true");
+            base.OnConfiguring(optionsBuilder);
+        }
     }
 }
